d478cf58f2de8b6adda6a2a3af72ebe9172fd01f
Harry Nelken <Harry@Harrys-Mac-Pro.local>
2014-11-10 17:25:12 -0800

    Initial Commit

---
+++ Sampler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sampler
+//
+//  Created by Harry Nelken on 11/10/14.
+//  Copyright (c) 2014 Harry Nelken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
